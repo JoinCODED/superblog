716ed42c6bdafef6e6f1adf3b703816fe609885b
sentientazure <themshary94@gmail.com>
2019-06-23 17:25:20 +0300

    Cleaned up

--- articles/models.py
+++ articles/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-
-class Article(models.Model):
-    title = models.CharField(max_length=125)
-    body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
-    created = models.DateTimeField(auto_now_add=True)
-    draft = models.BooleanField(default=False)
-    published = models.DateTimeField(null=True)
-    slug = models.SlugField()
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    about = models.TextField(null=True, blank=True)
+# Create your models here.
