c7bb53b44bc6c06db7c8de3d2865cf26e474dc4f
sentientazure <themshary94@gmail.com>
2019-08-06 14:01:06 +0300

    Removed existing URLs and kept the default admin URL

--- superblog/urls.py
+++ superblog/urls.py
@@ -19,20 +19,4 @@ from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.article_list, name="article-list"),
-    path('articles/create/', views.article_create, name="article-create"),
-    path('articles/<slug:article_slug>/', views.detail, name="article-detail"),
-
-    path('drafts/', views.draft_list, name="draft-list"),
-    path('drafts/<slug:article_slug>/', views.draft_edit, name="draft-edit"),
-    path('drafts/<slug:article_slug>/delete/', views.draft_delete, name="draft-delete"),
-
-    path('login/', views.login_view, name="login"),
-    path('register/', views.register_view, name="register"),
-    path('logout/', views.logout_view, name="logout"),
-
-    path('profile/', views.profile, name="profile"),
-    path('profile/edit/', views.profile_edit, name="profile-edit"),
-    path('profiles/<str:username>/', views.author_profile, name="author-profile"),
 ]
